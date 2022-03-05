042678a3006b505ecd301d54b47d81a6e3a3e434
Bas Klinkhamer <bas@position-fixed.nl>
2022-03-05 16:34:47 +0100

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:wc/recommended',
+    'plugin:lit/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [ 'error', {
+      varsIgnorePattern: '^_',
+    }],
+    'array-bracket-spacing': [ 'error', 'always', {
+      arraysInArrays: false,
+      objectsInArrays: false,
+    }],
+    'comma-dangle': [ 'error', 'always-multiline'],
+    'comma-spacing': [ 'error', {
+      after: true,
+      before: false,
+    }],
+    indent: [ 'error', 2 ],
+    'key-spacing': [ 'error', {
+      afterColon: true,
+      beforeColon: false,
+    }],
+    'linebreak-style': [ 'error', 'unix' ],
+    'max-len': [ 'error', {
+      code: 100,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true,
+      tabWidth: 2,
+    }],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [ 'error', 'always', {
+      arraysInObjects: false,
+      objectsInObjects: false,
+    }],
+    'object-property-newline': [ 'error', {
+      allowAllPropertiesOnSameLine: true,
+    }],
+    quotes: [ 'error', 'single' ],
+    semi: [ 'error', 'always' ],
+    'sort-imports': [ 'error', {
+      allowSeparatedGroups: true,
+      ignoreCase: true,
+      memberSyntaxSortOrder: [
+        'all',
+        'single',
+        'multiple',
+        'none',
+      ]
+    }],
+    'sort-keys': [ 'error', 'asc', {
+      caseSensitive: true,
+      natural: false,
+      minKeys: 3,
+    }]
+  },
+};
