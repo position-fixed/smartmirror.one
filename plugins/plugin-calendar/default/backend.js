75193affa7be68621839816358a104eafac89ff2
Bas Klinkhamer <bas@position-fixed.nl>
2022-07-03 18:53:23 +0200

    Add the plugin-calendar

---
+++ plugins/plugin-calendar/default/backend.js
@@ -0,0 +1,33 @@
+const gCalendarConnector = require('google-calendar-connector').default;
+
+async function getCalendarItems(inputs) {
+  const connector = new gCalendarConnector({
+    clientId: inputs.clientId,
+    clientSecret: inputs.clientSecret,
+    returnUrl: inputs.returnUrl,
+  });
+
+  try {
+    const items = await connector.getCalendarItems({
+      calendarIds: inputs.calendarIds.split(';'),
+      maxResults: 50,
+      token: inputs.token,
+    });
+    return items;
+  } catch(e) {
+    console.error(e);
+    return [];
+  }
+}
+
+/** @type {import('../types').BackendFunctions} */
+module.exports = {
+  async init({ inputs }) {
+    console.log('Init');
+    return { calendarItems: await getCalendarItems(inputs) };
+  },
+  async update({ inputs }) {
+    console.log('Update');
+    return { calendarItems: await getCalendarItems(inputs) };
+  },
+};
